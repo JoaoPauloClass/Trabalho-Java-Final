05beb58a3570e25d8611b7051d43e0b9a9d74fb9
GustavoGogola91 <90578528+GustavoGogola91@users.noreply.github.com>
2024-06-18 19:36:09 -0300

    bug fix

--- src/controller/PlayerController.java
+++ src/controller/PlayerController.java
@@ -6,7 +6,7 @@ import model.player.Player;
 public abstract class PlayerController {
     
 
-    public Player calculateAtributes(Player player) {
+    public static Player calculateAtributes(Player player) {
 
         Player tempPlayer = new Player();
 
@@ -20,15 +20,15 @@ public abstract class PlayerController {
 
         tempPlayer.setDefense(player.getDefense() + equipedArmor.getDefenseUpgrade());
 
-        if (equipedArmor.getTypeUpgrade() == "ATTACK") {
+        if (equipedArmor.getTypeUpgrade().equals("ATTACK")) {
             tempPlayer.setAttack(player.getAttack() + equipedArmor.getStatUpgrade());
-        } else if (equipedArmor.getTypeUpgrade() == "DEFENSE") {
+        } else if (equipedArmor.getTypeUpgrade().equals("DEFENSE")) {
             tempPlayer.setDefense(player.getDefense() + equipedArmor.getStatUpgrade());
-        }else if (equipedArmor.getTypeUpgrade() == "HEALTH") {
+        }else if (equipedArmor.getTypeUpgrade().equals("HEALTH")) {
             tempPlayer.setHealth(player.getHealth() + equipedArmor.getStatUpgrade());
-        } else if (equipedArmor.getTypeUpgrade() == "AGILITY") {
+        } else if (equipedArmor.getTypeUpgrade().equals("AGILITY")) {
             tempPlayer.setAgility(player.getAgility() + equipedArmor.getStatUpgrade());
-        } else if (equipedArmor.getTypeUpgrade() == "MANA") {
+        } else if (equipedArmor.getTypeUpgrade().equals("MANA")) {
             tempPlayer.setMana(player.getMana() + equipedArmor.getStatUpgrade());
         }
 
