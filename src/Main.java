62dc2f85dfc6c61801540af855aaaa406105ea6d
GustavoGogola91 <90578528+GustavoGogola91@users.noreply.github.com>
2024-06-21 21:12:53 -0300

    tests

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,10 @@
-import model.monster.*;
-import model.monster.boss.*;
-import model.player.Player;
+import controller.FloorController;
+import model.Floor;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-       
-        // * debugging purposes, will be removed in the merge
-        Skeleton monster = new Skeleton();
-        System.out.println(monster.getSprite());
 
-                //SystemGame.introduction();
-               /* try{
-                    p.addPoints();
-                }catch(Exception e){
-                    System.out.println(e.getMessage());
-                }
-                */
-              
-                
-                //GraphicalCombatSystem.playerTable(p,10);
-                
-                
-                
-        
 
-            }
-        }
-
+    }
+}
